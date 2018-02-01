2003841616fb08354cb5cc1599b617b63be7006e
Wojtek Nadara <wojtek.nadara@gmail.com>
2018-02-01 20:45:38 +0100

    Added winning frequency to stats

--- dom/ProduceWcolStats.py
+++ dom/ProduceWcolStats.py
@@ -64,6 +64,7 @@ for table_ind in range(0, 3):
     sys.stdout.write('progs_apx_ratio,')
     progs_in_table = progs
   
+  best_freq = [[0 for _ in range(len(progs_in_table))] for _ in range(max_rad + 1)]
   apx_ratio = [[0 for _ in range(len(progs_in_table))] for _ in range(max_rad + 1)]
   for row_id in idx:
     rad = df['rad'][row_id]
@@ -71,11 +72,21 @@ for table_ind in range(0, 3):
     for prog_id in range(len(progs_in_table)):
       prog = progs_in_table[prog_id]
       best = min(best, df[prog][row_id])
+    cnt_best = 0
+    for prog_id in range(len(progs_in_table)):
+      prog = progs_in_table[prog_id]
+      if best == df[prog][row_id]:
+        cnt_best += 1
     for prog_id in range(len(progs_in_table)):
       prog = progs_in_table[prog_id]
       if rad != 1:
         apx_ratio[0][prog_id] += 1. * df[prog][row_id] / best
+        if df[prog][row_id] == best:
+          best_freq[0][prog_id] += 1. / cnt_best
       apx_ratio[rad][prog_id] += 1. * df[prog][row_id] / best
+      if df[prog][row_id] == best:
+        best_freq[rad][prog_id] += 1. / cnt_best
+        
   
   for prog_id in range(len(progs_in_table)):
     sys.stdout.write(progs_in_table[prog_id])
@@ -94,3 +105,31 @@ for table_ind in range(0, 3):
       if prog_id != len(progs_in_table) - 1:
         sys.stdout.write(',')
     print ("")
+  
+  print ("")
+  if table_ind == 0:
+    sys.stdout.write('berlins_best_freq,')
+  elif table_ind == 1:
+    sys.stdout.write('felixs_best_freq,')
+  else:
+    sys.stdout.write('progs_best_freq,')
+  
+  for prog_id in range(len(progs_in_table)):
+    sys.stdout.write(progs_in_table[prog_id])
+    if prog_id != len(progs_in_table) - 1:
+      sys.stdout.write(',')
+  print("")
+  for rad in range(max_rad + 1):
+    if rad == 1:
+      continue
+    if rad == 0:
+      sys.stdout.write("all,")
+    else:
+      sys.stdout.write(str(rad) + ",")
+    for prog_id in range(len(progs_in_table)):
+      sys.stdout.write(str("%.3f"%(1. * best_freq[rad][prog_id])))
+      if prog_id != len(progs_in_table) - 1:
+        sys.stdout.write(',')
+    print ("")
+  
+  
