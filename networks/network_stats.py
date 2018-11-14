c7bfb25134a69b6e3b9dc178636fbc1764e994db
Felix Reidl <felix.reidl@gmail.com>
2018-11-14 11:54:15 +0000

    Data merging done

--- networks/network_stats.py
+++ networks/network_stats.py
@@ -7,16 +7,26 @@ from collections import defaultdict
 def compute_stats(filename):
     degrees = defaultdict(int)
     edges = set()
+    numerrors = 0
     with open(filename, 'r') as filebuf:
-        for l in filebuf:
+        for i, l in enumerate(filebuf):
             if l[0] in ['#', '%']:
                 continue
-            s,t = map(int, l[:-1].split())
+            if l[-1] == '\n':
+                l = l[:-1]
+            try:
+                s,t = map(int, l.split())
+            except:
+                numerrors += 1
+                continue
+
             if (s,t) in edges:
                 continue
             edges.add((s,t))
             degrees[s] += 1
             degrees[t] += 1
+    if numerrors > 1: # Some files have trailing edge-count
+        raise Exception("Too many errors")
     n = len(degrees)
     m = len(edges)
     avg_deg = 2*m / n
@@ -24,13 +34,16 @@ def compute_stats(filename):
     return n, m, avg_deg, max_deg
 
 
-files = glob.glob('small/*.txtg')
-files += glob.glob('medium/*.txtg')
-files += glob.glob('big/*.txtg')
-files += glob.glob('huge/*.txtg')
+files = glob.glob('*/*.txtg')
 
 print("test;n;m;avg_deg;max_deg")
 for f in files:
     name, _ = os.path.splitext(os.path.basename(f))
-    n, m, avg_deg, max_deg = compute_stats(f)
-    print("{};{};{};{:.2f};{}".format(name,n,m,avg_deg,max_deg))
+    try:
+        n, m, avg_deg, max_deg = compute_stats(f)
+        print("{};{};{};{:.2f};{}".format(name,n,m,avg_deg,max_deg))
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        print("Error in reading file", f)
+        raise
